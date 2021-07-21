b6b87ca189d32729fc78a3f423f52ecfbda8ba59
Juntai Park <juntai81@gmail.com>
2021-07-21 10:51:50 +0900

    fix: site.config (remove linkedin)

--- site.config.js
+++ site.config.js
@@ -19,7 +19,7 @@ module.exports = {
   // social usernames (optional)
   twitter: 'rkjun',
   github: 'rkjun',
-  linkedin: 'rkjun',
+  linkedin: '',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
